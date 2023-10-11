fa4083c88fd465229108af783e8fdf4a76150087
Beta Nurul Awwalin <beta.n.awwalin@gmail.com>
2023-10-11 13:36:21 +0700

    update dashboard

--- assets/style/script.js
+++ assets/style/script.js
@@ -1,3 +1,14 @@
+// Menggunakan JavaScript untuk memuat dan menampilkan sidebar
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("sidebar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("side_nav-placeholder").innerHTML = data;
+        });
+});
+
+
+
 $(".sidebar ul li").on("click", function () {
     $(".sidebar ul li.active").removeClass("active");
     $(this).addClass("active");
